d1a4645d1fa33fbae17aa1ae75a583786fc5a631
小怡白耶 <3530953858@qq.com>
2022-06-27 20:06:14 +0800

    feat: 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,18 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': 'off',
-    'no-unused-vars': 'off'
-  }
+    'no-tabs': 'warn',
+    'vue/multi-word-component-names': 'off'
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
